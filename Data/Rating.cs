d4f674dfd989beee7e86b78eaf54c764b93486a6
Eleven Fifty Coding <1150Coding@jinns-Mac-mini.local>
2023-05-02 09:50:16 -0400

    initial commit

---
+++ Data/Rating.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantRaterMVC.Data
+{
+    public class Rating
+    {
+        public int Id { get; set; }
+        public int RestuarantId { get; set; }
+        public double FoodScore { get; set; }
+        public double CleanlinessScore { get; set; }
+        public double AtmosphereScore { get; set; }
+        public virtual Restuarant Restuarant { get; set; }
+    }
+}
---
+++ Data/Restaurant.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantRaterMVC.Data
+{
+    public class Restaurant
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+        [Required]
+        [MaxLength]
+        public string Location { get; set; }
+        public virtual List<Rating> Ratings { get; set; } = new List<Rating>;
+
+        public double AverageFoodScore
+        {
+            get{
+                return Ratings.Count > 0 ? Ratings.Select(r => r.FoodScore).Sum() / Ratings.Count : 0;
+            }
+        }
+
+        public double AverageCleanlinessScore
+        {
+            get{
+                return Ratings.Count > 0 ? Ratings.Select(r => r.CleanlinessScore).Sum() / Ratings.Count : 0;
+            }
+        }
+
+          public double AverageAtmosphereScore
+        {
+            get{
+                return Ratings.Count > 0 ? Ratings.Select(r => r.AtmosphereScore).Sum() / Ratings.Count : 0;
+
+            }
+        }
+
+        public double Score
+        {
+            get{
+                return (AverageFoodScore + AverageCleanlinessScore + AverageAtmosphereScore) / 3;
+            }
+        }
+    
+    }
+}
